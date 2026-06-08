d45289acf1e9d7555dece26a2e745846b433a884
putri1011 <putriamelina624@gail.com>
2026-06-09 02:26:29 +0700

    Update backend project

--- app/Models/pegawai.php
+++ app/Models/pegawai.php
@@ -2,48 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Pegawai extends Model
 {
-    use HasFactory;
-
     protected $table = 'pegawais';
-
-    protected $primaryKey = 'id';
-
     protected $fillable = [
-        'nip',
-        'nama',
-        'jabatan',
-        'divisi',
-        'jatah_cuti',
-        'sisa_cuti',
-        'status',
+    'id_user',
+    'nip',
+    'nama',
+    'jabatan',
+    'divisi',
+    'jatah_cuti'        
     ];
-
-    // RELASI ABSENSI
-    public function absensis()
-    {
-        return $this->hasMany(
-            Absensi::class,
-            'pegawai_id'
-        );
-    }
-
-    // RELASI PENGAJUAN
-    public function pengajuans()
-    {
-        return $this->hasMany(
-            Pengajuan::class,
-            'pegawai_id'
-        );
-    }
-
-    // RELASI USER
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
 }
